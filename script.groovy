7c76e160319e3936ebfde006d520a0757fe532eb
Samson Ojo <samsonojoMOOC@users.noreply.github.com>
2025-05-20 03:36:20 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,9 +7,9 @@ def buildImage(){
     
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t samsonojo/demo-app:jmal-2.0 .'
+        sh 'docker build -t samsonojo/demo-app:jmal-2.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'socker push samsonojo/demo-app:jmal-2.0'                    }
+        sh 'docker push samsonojo/demo-app:jmal-2.1'                    }
 }
 
 def deployApp(){
